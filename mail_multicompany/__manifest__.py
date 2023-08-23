46939ca32fe8b1b6b0777c469c08fa2741088fe5
oimran135 <osamaimran135@gmail.com>
2023-08-23 17:52:25 +0500

    [ADD] multi-company module for mail servers

--- mail_multicompany/__manifest__.py
+++ mail_multicompany/__manifest__.py
@@ -1,16 +1,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Email Gateway Multi company",
-    "version": "15.0.0.0.0",
+    "version": "14.0.0.1.0",
     "category": "Extra Tools",
-    "author": "AxiomWorld",
-    "website": "www.axiomworld.net",
+    "author": "Odoo Community Association (OCA), " "Comunitea",
+    "website": "https://github.com/OCA/multi-company",
     "license": "AGPL-3",
-    "depends": ["mail"],
-    "data": [
-        "security/mail_security.xml",
-        "views/ir_mail_server_view.xml",
-        "views/fetchmail_view.xml",
-    ],
+    "depends": ["mail", "branch"],
+    "data": ["security/mail_security.xml", "views/ir_mail_server_view.xml"],
     "installable": True,
 }
--- mail_multicompany/models/fetchmail.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api
-
-
-class FetchMail(models.Model):
-    _inherit = 'fetchmail.server'
-
-    company_id = fields.Many2one("res.company", "Company")
