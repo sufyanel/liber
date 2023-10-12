a804475ef0f287cc90b52e0f1088c6d3328c3826
oimran135 <osamaimran135@gmail.com>
2023-10-12 16:06:40 +0500

    Further exceptions to figure issue

--- google_contacts_sync/controllers/google_oauth2.py
+++ google_contacts_sync/controllers/google_oauth2.py
@@ -85,101 +85,105 @@ class GoogleOAuthController(http.Controller):
 
     @staticmethod
     def sync_google_data(data):
-        logger.info(".....Now about to Sync Google Data.....")
-        google_contacts = request.env['res.partner'].search([('is_google_contact', '=', True)])
 
-        # Creating every possible labels of Google in google.labels model, they will be unique by their name
-        for label in data[2]:
-            google_labels = request.env['google.labels'].search([]).mapped('name')
-            if label not in google_labels and label != 'myContacts':
-                request.env['google.labels'].create({
-                    'name': label,
-                })
-        logger.info(".....Now about to Sync Customers.....")
-        for rec in data[1]:
-            if "Customer" in rec.get('label_names', []) or "Vendor" in rec.get('label_names', []) \
-                    or "Lead" in rec.get('label_names', []):
-                # Avoiding replication of same contacts, filtering by their email and name
-                if rec.get('emailAddresses'):
-                    email = rec.get('emailAddresses', [])[0].get('value')
-                else:
-                    email = "None"
-                if rec.get('names'):
-                    name = rec.get('names', [])[0].get('displayName')
-                else:
-                    name = "None"
+        try:
+            logger.info(".....Now about to Sync Google Data.....")
+            google_contacts = request.env['res.partner'].search([('is_google_contact', '=', True)])
 
-                if not email in google_contacts.mapped('name') or not name in google_contacts.mapped('email'):
-                    # Every contact should be a person, given google contact flag and address set to private address
-                    vals = {'company_type': 'person', 'is_google_contact': True, 'type': 'contact'}
-                    names = rec.get('names', [])
-                    if names:
-                        vals['name'] = names[0].get('displayName')
-                    emails = rec.get('emailAddresses', [])
-                    if emails:
-                        vals['email'] = emails[0].get('value')
+            # Creating every possible labels of Google in google.labels model, they will be unique by their name
+            for label in data[2]:
+                google_labels = request.env['google.labels'].search([]).mapped('name')
+                if label not in google_labels and label != 'myContacts':
+                    request.env['google.labels'].create({
+                        'name': label,
+                    })
+            logger.info(".....Now about to Sync Customers.....")
+            for rec in data[1]:
+                if "Customer" in rec.get('label_names', []) or "Vendor" in rec.get('label_names', []) \
+                        or "Lead" in rec.get('label_names', []):
+                    # Avoiding replication of same contacts, filtering by their email and name
+                    if rec.get('emailAddresses'):
+                        email = rec.get('emailAddresses', [])[0].get('value')
+                    else:
+                        email = "None"
+                    if rec.get('names'):
+                        name = rec.get('names', [])[0].get('displayName')
+                    else:
+                        name = "None"
 
-                    # Dividing addresses based on 5 different fields of odoo for address, state is not provided by
-                    # Google
-                    addresses = rec.get('addresses', [])
-                    if addresses:
-                        vals['street'] = addresses[0].get('streetAddress')
-                        vals['street2'] = addresses[0].get('extendedAddress')
-                        vals['city'] = addresses[0].get('city')
-                        vals['zip'] = addresses[0].get('postalCode')
-                        vals['country_id'] = request.env['res.country'].search(
-                            [('code', '=', addresses[0].get('country'))]).id
+                    if not email in google_contacts.mapped('name') or not name in google_contacts.mapped('email'):
+                        # Every contact should be a person, given google contact flag and address set to private address
+                        vals = {'company_type': 'person', 'is_google_contact': True, 'type': 'contact'}
+                        names = rec.get('names', [])
+                        if names:
+                            vals['name'] = names[0].get('displayName')
+                        emails = rec.get('emailAddresses', [])
+                        if emails:
+                            vals['email'] = emails[0].get('value')
 
-                    # taking image url converting it into bytes and then encoding it into base64
-                    photos = rec.get('photos', [])
-                    if photos:
-                        photo = photos[0].get('url')
-                        response = requests.get(photo, stream=True)
-                        if response.status_code == 200:
-                            base64encoded = base64.b64encode(response.content)
-                            vals['image_1920'] = base64encoded
-                        else:
-                            raise ValidationError(_(f"Failed to download image from URL: {photo}"))
-                else:
-                    continue
+                        # Dividing addresses based on 5 different fields of odoo for address, state is not provided by
+                        # Google
+                        addresses = rec.get('addresses', [])
+                        if addresses:
+                            vals['street'] = addresses[0].get('streetAddress')
+                            vals['street2'] = addresses[0].get('extendedAddress')
+                            vals['city'] = addresses[0].get('city')
+                            vals['zip'] = addresses[0].get('postalCode')
+                            vals['country_id'] = request.env['res.country'].search(
+                                [('code', '=', addresses[0].get('country'))]).id
 
-                # getting unique phone number values for phone and mobile in odoo, if type isn't mentioned or anyone
-                # of them is missing, first phonenumbers will be picked
-                phone_numbers = rec.get('phoneNumbers', [])
-                if phone_numbers and any(numbers.get('type') for numbers in phone_numbers):
-                    for number in phone_numbers:
-                        if number.get('type') == 'home':
-                            vals['phone'] = number.get('canonicalForm')
-                        elif number.get('type') == 'mobile':
-                            vals['mobile'] = number.get('canonicalForm')
-                    if 'phone' not in vals:
+                        # taking image url converting it into bytes and then encoding it into base64
+                        photos = rec.get('photos', [])
+                        if photos:
+                            photo = photos[0].get('url')
+                            response = requests.get(photo, stream=True)
+                            if response.status_code == 200:
+                                base64encoded = base64.b64encode(response.content)
+                                vals['image_1920'] = base64encoded
+                            else:
+                                raise ValidationError(_(f"Failed to download image from URL: {photo}"))
+                    else:
+                        continue
+
+                    # getting unique phone number values for phone and mobile in odoo, if type isn't mentioned or anyone
+                    # of them is missing, first phonenumbers will be picked
+                    phone_numbers = rec.get('phoneNumbers', [])
+                    if phone_numbers and any(numbers.get('type') for numbers in phone_numbers):
+                        for number in phone_numbers:
+                            if number.get('type') == 'home':
+                                vals['phone'] = number.get('canonicalForm')
+                            elif number.get('type') == 'mobile':
+                                vals['mobile'] = number.get('canonicalForm')
+                        if 'phone' not in vals:
+                            vals['phone'] = phone_numbers[0].get('canonicalForm')
+                        if 'mobile' not in vals and len(phone_numbers) > 1:
+                            vals['phone'] = phone_numbers[1].get('canonicalForm')
+                    elif phone_numbers:
                         vals['phone'] = phone_numbers[0].get('canonicalForm')
-                    if 'mobile' not in vals and len(phone_numbers) > 1:
-                        vals['phone'] = phone_numbers[1].get('canonicalForm')
-                elif phone_numbers:
-                    vals['phone'] = phone_numbers[0].get('canonicalForm')
-                    if len(phone_numbers) > 1:
-                        vals['mobile'] = phone_numbers[1].get('canonicalForm')
+                        if len(phone_numbers) > 1:
+                            vals['mobile'] = phone_numbers[1].get('canonicalForm')
 
-                # organizations giving away both job position as title and company as its name, company id will be
-                # searched if found, it will be set as parent by default
-                organizations = rec.get('organizations', [])
-                if organizations:
-                    company = organizations[0].get('name')
-                    vals['parent_id'] = request.env['res.partner'].search([('name', '!=', False)]).filtered(
-                        lambda x: x.name.lower() == company.lower()).id or False
-                    vals['function'] = organizations[0].get('title')
+                    # organizations giving away both job position as title and company as its name, company id will be
+                    # searched if found, it will be set as parent by default
+                    organizations = rec.get('organizations', [])
+                    if organizations:
+                        company = organizations[0].get('name')
+                        vals['parent_id'] = request.env['res.partner'].search([('name', '!=', False)]).filtered(
+                            lambda x: x.name.lower() == company.lower()).id or False
+                        vals['function'] = organizations[0].get('title')
 
-                # labels are actually groups which are termed as labels in google contacts to sort every contact
-                # label wise
-                labels = rec.get('label_names', [])
-                if labels:
-                    all_matching_labels_ids = request.env['google.labels'].search([('name', 'in', labels)])
-                    vals['google_label_ids'] = all_matching_labels_ids.ids
-                request.env['res.partner'].create(vals)
-                logger.info(".....End of the Entire Process.....")
-            else:
-                pass
+                    # labels are actually groups which are termed as labels in google contacts to sort every contact
+                    # label wise
+                    labels = rec.get('label_names', [])
+                    if labels:
+                        all_matching_labels_ids = request.env['google.labels'].search([('name', 'in', labels)])
+                        vals['google_label_ids'] = all_matching_labels_ids.ids
+                    request.env['res.partner'].create(vals)
+                    logger.info(".....End of the Entire Process.....")
+                else:
+                    pass
+        except Exception as e:
+            raise e
 
     @http.route('/oauth/contacts', type='http', auth='public', website=True)
     def oauth_contacts_sync(self, **kwargs):
