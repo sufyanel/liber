092c365dfcd36cfd520148370d80c5c67099fc24
oimran135 <osamaimran135@gmail.com>
2023-10-18 12:06:27 +0500

    logical changes in sync contacts

--- google_contacts_sync/controllers/google_oauth2.py
+++ google_contacts_sync/controllers/google_oauth2.py
@@ -19,8 +19,6 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
 
 logger = logging.getLogger(__name__)
-logging.getLogger('googleapicliet.discovery_cache').setLevel(logging.ERROR)
-
 
 class GoogleOAuthController(http.Controller):
 
@@ -48,41 +46,33 @@ class GoogleOAuthController(http.Controller):
     
                 connections.extend(results.get('connections', []))
 
-            for contact in connections:
-                memberships = contact.get('memberships', [])
-                label_names = []
-                # logger.info(".....Fetching Labels Now.....")
-                logger.info(f".....Memberships: {memberships}")
-                for membership in memberships:
+            resource_names = list()
+            groups = service.contactGroups().list().execute()
+            groups = groups.get('contactGroups')
 
-                    if 'contactGroupMembership' not in membership:
+            for group in groups:
+                if group.get('name') in ['Vendor', 'Lead', 'Customer']:
+                    resource_names.append(group.get('resourceName'))
+                    google_labels.append(group)
+
+            valid_contacts = list()
+            for connection in connections:
+                memberships = connection.get('memberships')
+                if memberships:
+                    contact_groups = memberships.get('contactGroupMembership')
+                    if not contact_groups:
                         continue
-
-                    if 'contactGroupId' not in membership['contactGroupMembership']:
-                        continue
-
-                    contact_group_id = membership['contactGroupMembership']['contactGroupId']
-                    logger.info(f".....Contact Group: {contact_group_id}")
-                    # Fetch the contact group details using the contact_group_id
-                    group_details = service.contactGroups().get(
-                        resourceName=f'contactGroups/{contact_group_id}'
-                    ).execute()
-                    logger.info(f".....Groups: {group_details}")
-                    label = group_details.get('name', '')
-
-                    # Get the label name and append it to label_names list
-                    label_names.append(label)
-
-                    # creating another list so can create separate label records avoiding duplication
-                    if label not in google_labels:
-                        google_labels.append(label)
-                # Add the label_names list to the contact dictionary
-                # logger.info(f".....Label Names: {label_names}")
-                contact['label_names'] = label_names
-                # logger.info(".....Labels Fetched Successfully.....")
-            data.append(connections)
-            data.append(google_labels)
-            GoogleOAuthController.sync_google_data(data)
+                else:
+                    continue
+                labels = list()
+                for group in contact_groups:
+                    for label in google_labels:
+                        if group.get('contactGroupResourceName') == label.get("resourceName"):
+                            labels.append(label.get("name"))
+                if labels:
+                    connection["google_labels"] = labels
+                    valid_contacts.append(connection)
+            GoogleOAuthController.sync_google_data(valid_contacts)
         except Exception as e:
             raise e
 
@@ -93,49 +83,26 @@ class GoogleOAuthController(http.Controller):
             logger.info(".....Now about to Sync Google Data.....")
             google_contacts = request.env['res.partner'].search([('is_google_contact', '=', True)])
 
-            # Creating every possible labels of Google in google.labels model, they will be unique by their name
-            logger.info(f"Data: {data[1]}")
-            for label in data[2]:
-                google_labels = request.env['google.labels'].search([]).mapped('name')
-                if label not in google_labels and label != 'myContacts':
-                    request.env['google.labels'].create({
-                        'name': label,
-                    })
-                print(label)
-            logger.info(".....Now about to Sync Customers.....")
-            for rec in data[1]:
-                if "Customer" in rec.get('label_names', []) or "Vendor" in rec.get('label_names', []) \
-                        or "Lead" in rec.get('label_names', []):
-                    # Avoiding replication of same contacts, filtering by their email and name
-                    if rec.get('emailAddresses'):
-                        email = rec.get('emailAddresses', [])[0].get('value')
-                    else:
-                        email = "None"
-                    if rec.get('names'):
-                        name = rec.get('names', [])[0].get('displayName')
-                    else:
-                        name = "None"
+            for rec in data:
+                # Avoiding replication of same contacts, filtering by their email and name
+                if rec.get('emailAddresses'):
+                    email = rec.get('emailAddresses', [])[0].get('value')
+                else:
+                    email = "None"
+                if rec.get('names'):
+                    name = rec.get('names', [])[0].get('displayName')
+                else:
+                    name = "None"
 
-                    if not email in google_contacts.mapped('name') or not name in google_contacts.mapped('email'):
-                        # Every contact should be a person, given google contact flag and address set to private address
-                        vals = {'company_type': 'person', 'is_google_contact': True, 'type': 'contact'}
-                        names = rec.get('names', [])
-                        if names:
-                            vals['name'] = names[0].get('displayName')
-                        emails = rec.get('emailAddresses', [])
-                        if emails:
-                            vals['email'] = emails[0].get('value')
-
-                        # Dividing addresses based on 5 different fields of odoo for address, state is not provided by
-                        # Google
-                        addresses = rec.get('addresses', [])
-                        if addresses:
-                            vals['street'] = addresses[0].get('streetAddress')
-                            vals['street2'] = addresses[0].get('extendedAddress')
-                            vals['city'] = addresses[0].get('city')
-                            vals['zip'] = addresses[0].get('postalCode')
-                            vals['country_id'] = request.env['res.country'].search(
-                                [('code', '=', addresses[0].get('country'))]).id
+                if name not in google_contacts.mapped('name') or email not in google_contacts.mapped('email'):
+                    # Every contact should be a person, given google contact flag and address set to private address
+                    vals = {'company_type': 'person', 'is_google_contact': True, 'type': 'contact'}
+                    names = rec.get('names', [])
+                    if names:
+                        vals['name'] = names[0].get('displayName')
+                    emails = rec.get('emailAddresses', [])
+                    if emails:
+                        vals['email'] = emails[0].get('value')
 
                         # taking image url converting it into bytes and then encoding it into base64
                         photos = rec.get('photos', [])
@@ -177,16 +144,14 @@ class GoogleOAuthController(http.Controller):
                             lambda x: x.name.lower() == company.lower()).id or False
                         vals['function'] = organizations[0].get('title')
 
-                    # labels are actually groups which are termed as labels in google contacts to sort every contact
-                    # label wise
-                    labels = rec.get('label_names', [])
-                    if labels:
-                        all_matching_labels_ids = request.env['google.labels'].search([('name', 'in', labels)])
-                        vals['google_label_ids'] = all_matching_labels_ids.ids
-                    request.env['res.partner'].create(vals)
-                    logger.info(".....End of the Entire Process.....")
-                else:
-                    pass
+                # labels are actually groups which are termed as labels in google contacts to sort every contact
+                # label wise
+                labels = rec.get('google_labels', [])
+                if labels:
+                    all_matching_labels_ids = request.env['google.labels'].search([('name', 'in', labels)])
+                    vals['google_label_ids'] = all_matching_labels_ids.ids
+                request.env['res.partner'].create(vals)
+                logger.info(".....End of the Entire Process.....")
         except Exception as e:
             raise e
 
