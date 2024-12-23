5eb0ef5533696782e0d7bed95b12a5fc017311c6
hasnain <hasnain.mustafa@axiomworld.net>
2024-12-23 17:26:39 +0500

    Removed all 15 custom to upgrade database to 17

--- asset_number_customization/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import product_template_ext
-from . import customer_part_number
-from . import sale_order_line_ext
--- bulk_sales_order_confirmation/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2020-22 Manish Kumar Bohra <manishkumarbohra@outlook.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/Lgpl-3.0.html
-
-from . import model
